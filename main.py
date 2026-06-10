60b12d211eb98a200b1ae3d5d02d2597b28fb02f
nguyentyennhi90-lang <nguyentyennhi90@dtu.edu.vn>
2026-06-10 22:26:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def tinh_thue_thu_nhap(thu_nhap):
-    thu_nhap_tinh_thue= thu_nhap - 4000000
-    return max(0, thu_nhap_tinh_thue * 0.1)
+def tinh_diem_gpa(diem_so):
+    return 0.0
+
+print("Điểm GPA hệ 4 là:", tinh_diem_gpa(8.5))
